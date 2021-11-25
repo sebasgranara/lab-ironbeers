5d83ce972c0c5d41c4cf18e9c47ca2b0ccee773a
Sebas Granara <sebastian.granara@gmail.com>
2021-11-25 18:01:30 +0100

    hasta parte de interaccion 3

--- app.js
+++ app.js
@@ -22,4 +22,17 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+//app.get("/", (req, res, next) => res.render("index"));
+ 
+app.get("/beers", (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+  .catch(error => console.log(error));
+
+  res.render("beers");
+});
+ 
+app.get("/random-beer", (req, res, next) => res.render("random-beer"));
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
